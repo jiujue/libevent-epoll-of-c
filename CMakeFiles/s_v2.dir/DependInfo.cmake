5cb91b3a9886fd56c0869fcab9ff67362a71c40f
jiujue - ubuntu18 <jiujueismmp@qq.com>
2020-02-04 11:53:09 +0800

    s_v2

---
+++ CMakeFiles/s_v2.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/libevent-epoll-of-c/src/get_file_type.c" "/root/libevent-epoll-of-c/CMakeFiles/s_v2.dir/src/get_file_type.c.o"
+  "/root/libevent-epoll-of-c/src/judge_isdir.c" "/root/libevent-epoll-of-c/CMakeFiles/s_v2.dir/src/judge_isdir.c.o"
+  "/root/libevent-epoll-of-c/src/libev.c" "/root/libevent-epoll-of-c/CMakeFiles/s_v2.dir/src/libev.c.o"
+  "/root/libevent-epoll-of-c/src/main.c" "/root/libevent-epoll-of-c/CMakeFiles/s_v2.dir/src/main.c.o"
+  "/root/libevent-epoll-of-c/src/send_dir.c" "/root/libevent-epoll-of-c/CMakeFiles/s_v2.dir/src/send_dir.c.o"
+  "/root/libevent-epoll-of-c/src/send_file.c" "/root/libevent-epoll-of-c/CMakeFiles/s_v2.dir/src/send_file.c.o"
+  "/root/libevent-epoll-of-c/src/send_html_head.c" "/root/libevent-epoll-of-c/CMakeFiles/s_v2.dir/src/send_html_head.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/s_v2.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/s_v2.dir/src/get_file_type.c.o"
+  "CMakeFiles/s_v2.dir/src/judge_isdir.c.o"
+  "CMakeFiles/s_v2.dir/src/libev.c.o"
+  "CMakeFiles/s_v2.dir/src/main.c.o"
+  "CMakeFiles/s_v2.dir/src/send_dir.c.o"
+  "CMakeFiles/s_v2.dir/src/send_file.c.o"
+  "CMakeFiles/s_v2.dir/src/send_html_head.c.o"
+  "s_v2.pdb"
+  "s_v2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/s_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
