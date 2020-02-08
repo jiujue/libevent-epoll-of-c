1302cdbc3deca8491f92daa7661d1d60b9e1519c
jiujue <a123123.j>
2020-02-08 21:07:06 +0800

    try fix 01

--- src/send_file.c
+++ src/send_file.c
@@ -15,7 +15,25 @@ int send_file(struct bufferevent *bufev,char* file){
     int read_len = 0;
 
     char * type = get_file_type(file);
-    send_html_head(bufev,200, "OK", type);
+
+//    send_html_head(bufev,200, "OK", type);
+//    int send_html_head(struct bufferevent* bufev, int stat_no, const char* stat_desc, char* type)
+    {
+        char buf[1024] = {0};
+        //send status row
+        sprintf(buf,"HTTP/1.1 %d %s \r\n",200,"ok");
+        bufferevent_write(bufev,buf,strlen(buf));
+
+//    printf("send htp head -> status line:%s \n",buf);
+
+        //send message dgrame
+        sprintf(buf,"Content-Type: %s; charset=utf-8\r\n",type);
+        bufferevent_write(bufev,buf,strlen(buf));
+//  printf("send htp head -> grame:%s",buf);
+
+        //send blank row
+        bufferevent_write(bufev,"\r\n",2);
+    }
 
     while((read_len=read(ffd, file_read_buf,sizeof(file_read_buf))) > 0)
     {
@@ -31,6 +49,6 @@ int send_file(struct bufferevent *bufev,char* file){
     close(ffd);
     printf("close ...\n");
     free(file_read_buf);
-    sleep(2);
+
     return 0;
 }
